45777f4d8b21a600ed8b3c97882a18193d244588
A64Danil <a64danil@mail.ru>
2018-04-28 23:51:18 +0300

    prepare for last loading

--- js/qoobloader.js
+++ js/qoobloader.js
@@ -1,5 +1,17 @@
-/**
- * Created by Danil on 26.04.2018.
- */
 
-console.log('Node are working. Тест русских симовлов');
+
+//console.log(wrapper);
+
+(function () {
+    const wrapper = document.querySelector('.qsvgl-wrapper');
+    console.log(wrapper);
+
+    window.addEventListener('load', function (){
+        console.log('типа всё загрузилось');
+    });
+
+    document.addEventListener('DOMContentLoaded', function (){
+        console.log('DOM готов к работе');
+    });
+
+})();
