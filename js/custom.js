ecb050bb43058d3675aba6c19d2b13dde883e9e2
A64Danil <a64danil@mail.ru>
2018-05-06 23:00:34 +0300

    replace code of logo to JS, add some funcs to add and remove elements

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,62 @@
  */
 
 console.log('Node are working. Тест русских симовлов');
+
+(function () {
+    function inherit(child, parent) {
+        var Temp = function () {};
+        Temp.prototype = parent.prototype;
+        child.prototype = new Temp();
+        console.log(child.constructor);
+        console.log(child.prototype);
+    }
+    
+    var F = function (name) {
+        this.name = name;
+    };
+    F.prototype.setName = function (name) {
+        this.name = name;
+    }
+
+    F.prototype.getName = function () {
+        return this.name;
+    }
+    
+    F.prototype.setAge = function (age) {
+        this.age = age;
+    }
+
+    F.prototype.getAge = function () {
+        return this.age;
+    }
+
+    var F2 = function (name, lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    };
+
+    //F2.prototype = F.prototype;
+
+    F2.prototype.getLastName = function () {
+        return this.lastName;
+    }
+
+    F2.prototype.setLastName = function (lastName) {
+        this.lastName = lastName;
+    }
+
+    var obj1 = new F('Анна'),
+        obj2 = new F2('Данил', 'Хрущев');
+
+    F2.prototype.smeh = "haha";
+
+    inherit(F2, F);
+    // obj1.setLastName('Хрущева');
+    // console.log(obj2.getName() + " " + obj2.getLastName());
+    // console.log(obj1.getName() + " " + obj1.getLastName());
+    // console.log(obj2.__proto__);
+    //
+    // obj1.setAge(20);
+    // console.log(obj1.getAge());
+    // console.log(obj2.smeh);
+})();
--- js/qoobloader.js
+++ js/qoobloader.js
@@ -1,13 +1,67 @@
+/*
+ * Created by Danil on 26.04.2018.
+ * Qoobeo Loader
+ * Display none after loading some content
+ *
+ */
 
-
-//console.log(wrapper);
+/*
+ * 1. Creating elements
+ * 2. Adding animation to page
+ * 3. Adding of event listeners
+ * 4. Remove animation after page load
+ *
+ */
 
 (function () {
-    const wrapper = document.querySelector('.qsvgl-wrapper');
-    console.log(wrapper);
+
+    function addElement(tag, className, place = document.body, before) {
+        var el = document.createElement(tag);
+        el.setAttribute('class', className);
+        if (before) {
+            document.body.insertBefore(el, place.firstChild);
+        }
+        else {
+            place.appendChild(el);
+        }
+    }
+
+    function addLogo() {
+        addElement('div', 'qsvgl-wrapper', document.body, 'before');
+        var qsvglWrap = document.querySelector('.qsvgl-wrapper');
+        addElement('div', 'qsvgl test2', qsvglWrap);
+        var qsvgl = document.querySelector('.qsvgl');
+        addElement('div', 'qsvgl-text', qsvglWrap);
+        var qsvglText = document.querySelector('.qsvgl-text');
+        qsvglText.textContent = 'Loading';
+        addElement('div', 'qsvgl-textDots', qsvglText);
+        var qsvglTextDots = document.querySelector('.qsvgl-textDots');
+        qsvglTextDots.textContent = '...';
+
+        addElement('div', 'line top_left', qsvgl);
+        addElement('div', 'line top_right', qsvgl);
+        addElement('div', 'line right', qsvgl);
+        addElement('div', 'line mid_right', qsvgl);
+        addElement('div', 'line mid', qsvgl);
+        addElement('div', 'line bot_right', qsvgl);
+        addElement('div', 'line bot_left', qsvgl);
+        addElement('div', 'line mid_left', qsvgl);
+        addElement('div', 'line left', qsvgl);
+    }
+
+    addLogo();
+
+    function removeLogo() {
+        var qsvglWrap = document.querySelector('.qsvgl-wrapper');
+        qsvglWrap.setAttribute('class', 'qsvgl-wrapper removed');
+        setTimeout(function () {
+            document.body.removeChild(qsvglWrap);
+        }, 1200);
+    }
 
     window.addEventListener('load', function (){
         console.log('типа всё загрузилось');
+        setTimeout(removeLogo, 2000);
     });
 
     document.addEventListener('DOMContentLoaded', function (){
@@ -15,3 +69,4 @@
     });
 
 })();
+
