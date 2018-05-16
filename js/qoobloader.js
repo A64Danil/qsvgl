941abd9f30224a1072159a91d92eb47ce0083aa1
A64Danil <a64danil@mail.ru>
2018-05-16 22:48:36 +0300

    refactor main JS file, now u can add some class to loader-wrapper, added css-clasees with diff animation

--- js/qoobloader.js
+++ js/qoobloader.js
@@ -14,63 +14,71 @@
  */
 
 function removeLoader() {
-    console.log('сейчас будем скрывать лоадер');
     var qsvglWrap = document.querySelector('.qsvgl-wrapper');
-    qsvglWrap.setAttribute('class', 'qsvgl-wrapper removed');
+    if (qsvglWrap != null) {
+        //console.log('now we remove loader');
+
+        qsvglWrap.className += " removed";
+        setTimeout(function () {
+            document.body.removeChild(qsvglWrap);
+            qsvglWrap = null;
+        }, 1100);
+    }
 }
 
-function showLoader() {
-    console.log('сейчас будем показывать лоадер');
+function showLoader(param) {
+    console.log('create loader now');
+    addLogo(param);
+}
+
+function addLogo(param) {
+    addElement('div', 'qsvgl-wrapper', document.body, 'before');
     var qsvglWrap = document.querySelector('.qsvgl-wrapper');
-    qsvglWrap.setAttribute('class', 'qsvgl-wrapper');
+
+    if (param) {
+        var tmp = qsvglWrap.getAttribute('class');
+        tmp += " " + param;
+        qsvglWrap.setAttribute('class', tmp);
+    }
+
+    addElement('div', 'qsvgl test2', qsvglWrap);
+    var qsvgl = document.querySelector('.qsvgl');
+    addElement('div', 'qsvgl-text', qsvglWrap);
+    var qsvglText = document.querySelector('.qsvgl-text');
+    qsvglText.textContent = 'Loading';
+    addElement('div', 'qsvgl-textDots', qsvglText);
+    var qsvglTextDots = document.querySelector('.qsvgl-textDots');
+    qsvglTextDots.textContent = '...';
+
+    addElement('div', 'line top_left', qsvgl);
+    addElement('div', 'line top_right', qsvgl);
+    addElement('div', 'line right', qsvgl);
+    addElement('div', 'line mid_right', qsvgl);
+    addElement('div', 'line mid', qsvgl);
+    addElement('div', 'line bot_right', qsvgl);
+    addElement('div', 'line bot_left', qsvgl);
+    addElement('div', 'line mid_left', qsvgl);
+    addElement('div', 'line left', qsvgl);
+}
+
+function addElement(tag, className, place = document.body, before) {
+    var el = document.createElement(tag);
+    el.setAttribute('class', className);
+    if (before) {
+        document.body.insertBefore(el, place.firstChild);
+    }
+    else {
+        place.appendChild(el);
+    }
 }
 
 (function () {
-
-    function addElement(tag, className, place = document.body, before) {
-        var el = document.createElement(tag);
-        el.setAttribute('class', className);
-        if (before) {
-            document.body.insertBefore(el, place.firstChild);
-        }
-        else {
-            place.appendChild(el);
-        }
-    }
-
-    function addLogo() {
-        addElement('div', 'qsvgl-wrapper', document.body, 'before');
-        var qsvglWrap = document.querySelector('.qsvgl-wrapper');
-        addElement('div', 'qsvgl test2', qsvglWrap);
-        var qsvgl = document.querySelector('.qsvgl');
-        addElement('div', 'qsvgl-text', qsvglWrap);
-        var qsvglText = document.querySelector('.qsvgl-text');
-        qsvglText.textContent = 'Loading';
-        addElement('div', 'qsvgl-textDots', qsvglText);
-        var qsvglTextDots = document.querySelector('.qsvgl-textDots');
-        qsvglTextDots.textContent = '...';
-
-        addElement('div', 'line top_left', qsvgl);
-        addElement('div', 'line top_right', qsvgl);
-        addElement('div', 'line right', qsvgl);
-        addElement('div', 'line mid_right', qsvgl);
-        addElement('div', 'line mid', qsvgl);
-        addElement('div', 'line bot_right', qsvgl);
-        addElement('div', 'line bot_left', qsvgl);
-        addElement('div', 'line mid_left', qsvgl);
-        addElement('div', 'line left', qsvgl);
-    }
-
-    addLogo();
-
-
-    
-
+    addLogo("whiteBg");
 
     window.addEventListener('load', function (){
         console.log('типа всё загрузилось');
-        // TODO: ремовЛого надо вызывать в том месте, где вызывается последний js
-        setTimeout(removeLoader, 2000);
+        // You need to call removeLoader in your own place, at the end of loading
+        setTimeout(removeLoader, 1000);
     });
 
     document.addEventListener('DOMContentLoaded', function (){
